8664f1064ce5f68f88277148fa9a95b83ff9e755
Alex Pelavin <ajpele@gmail.com>
2016-08-06 21:20:48 -0700

    Implemented custom commands

--- commands/text/help.js
+++ commands/text/help.js
@@ -1,19 +1,28 @@
 var sendMessage = require('../sendmessage.js');
+var fs = require('fs')
+var customs = JSON.parse(fs.readFileSync('custom.json').toString())
+
+function customCommandsText(commands) {
+    var keys = Object.keys(commands);
+    text = "Custom Commands:";
+    for(var key in keys){
+        text += "\n  -!"+keys[key];
+    }
+    return text;
+}
 
 module.exports = {
     regexp: [/^!help$/i],
     action: function(bot, message) {
         var text =
-        "Help:\n" + 
-        "\t- !agee: Brings bot lazzer into your channel for a cheerful \"agee\"\n" + 
-        "\t- !eightball: Answeres a yes or no questions you give bot lazzer\n" +
-        "\t- !fortune: Gives you a fortune cookie style fortune\n" +
-        "\t- !kappa: Puts a kappa face in the chat\n" +
-        "\t- !neverlucky: Expresses your frustration with RNGesus\n" +
-        "\t- !play [youtube link]:[start second]: Plays the audio for the given youtube video\n" +
-        "\t- !rng [min] to [max]: Generates a random number between the min and max\n" +
-        "\t- !shh: Stop the audio bot lazzer is playing\n" +
-        "";
+        "Commands:\n" + 
+        "  -!eightball: Answeres a yes or no questions you give bot lazzer\n" +
+        "  -!fortune: Gives you a fortune cookie style fortune\n" +
+        "  -!kappa: Puts a kappa face in the chat\n" +
+        "  -!play [youtube link]:[start second]: Plays the audio for the given youtube video\n" +
+        "  -!rng [min] to [max]: Generates a random number between the min and max\n" +
+        "  -!shh: Stop the audio bot lazzer is playing\n" +
+        customCommandsText(customs);
         sendMessage(bot, message.author, text);
     }
 }
--- commands/voice/agee.js
+++
@@ -1,8 +0,0 @@
-var playSound = require('../playsound.js');
-
-module.exports = {
-    regexp: [/^!agee+$/i],
-    action: function(bot, message) {
-        playSound(bot, message, 'resources/sounds/agee.m4a');
-    }
-}
