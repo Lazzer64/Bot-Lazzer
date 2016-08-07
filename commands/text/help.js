399f3903df267aae58b375ae898022214c854f34
Alex Pelavin <ajpele@gmail.com>
2016-08-07 00:28:06 -0700

    Added link to githubpage

--- commands/text/help.js
+++ commands/text/help.js
@@ -5,14 +5,15 @@ module.exports = {
     action: function(bot, message) {
         var text =
         "Help:\n" + 
-        "\t- !agee: Brings bot lazzer into your channel for a cheerful \"agee\"\n" + 
-        "\t- !eightball: Answeres a yes or no questions you give bot lazzer\n" +
-        "\t- !fortune: Gives you a fortune cookie style fortune\n" +
-        "\t- !kappa: Puts a kappa face in the chat\n" +
-        "\t- !neverlucky: Expresses your frustration with RNGesus\n" +
-        "\t- !play [youtube link]:[start second]: Plays the audio for the given youtube video\n" +
-        "\t- !rng [min] to [max]: Generates a random number between the min and max\n" +
-        "\t- !shh: Stop the audio bot lazzer is playing\n" +
+        "  -!agee: Brings bot lazzer into your channel for a cheerful \"agee\"\n" + 
+        "  -!eightball: Answeres a yes or no questions you give bot lazzer\n" +
+        "  -!fortune: Gives you a fortune cookie style fortune\n" +
+        "  -!kappa: Puts a kappa face in the chat\n" +
+        "  -!neverlucky: Expresses your frustration with RNGesus\n" +
+        "  -!play [youtube link]:[start second]: Plays the audio for the given youtube video\n" +
+        "  -!rng [min] to [max]: Generates a random number between the min and max\n" +
+        "  -!shh: Stop the audio bot lazzer is playing\n" +
+        "Fork me on github! https://github.com/Lazzer64/Bot-Lazzer\n" +
         "";
         sendMessage(bot, message.author, text);
     }
