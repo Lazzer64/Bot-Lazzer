8b6806540234b0fa0b27d1ce753ecc5f311fcd3b
Alex Pelavin <ajpele@gmail.com>
2016-08-10 20:10:30 -0700

    Added 'youtube' to permission of !play

--- commands/voice/play.js
+++ commands/voice/play.js
@@ -2,6 +2,7 @@ var playYoutube = require('../playyoutube.js');
 
 module.exports = {
     regexp: [/^!play \S*$/i],
+    permission: ['youtube'],
     action: function(bot, message) {
          playYoutube(bot, message);
     }
