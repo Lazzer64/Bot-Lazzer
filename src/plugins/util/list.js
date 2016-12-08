60f1417669fca00b32ba7cbf0cadd731eedc8f3b
Alex Pelavin <ajpele@gmail.com>
2016-12-07 23:50:18 -0800

    Fixed regexp in list.js

--- src/plugins/util/list.js
+++ src/plugins/util/list.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'list',
     description: 'Lists currently enabled commands',
     usage: 'list',
-    regexp: '[(list)(commands)]',
+    regexp: 'list|commands',
     action: function(message) {
         var commands = global.commands;
         var response = 'Available commands:\n```';
