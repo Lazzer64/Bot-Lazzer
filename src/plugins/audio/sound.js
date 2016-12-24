620f8f65e766d6ffc4696b0bfacef046bf2b0437
Alex Pelavin <ajpele@gmail.com>
2016-12-24 04:18:18 -0800

    Audio plugin

---
+++ src/plugins/audio/sound.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const sound_dir = `${__dirname}/sounds`
+const dir = fs.readdirSync(`${__dirname}/sounds`);
+
+var description = 'Plays a sound in the user\'s current voice channel';
+var regexp = '^(';
+var sounds = {};
+
+// Load sound files from sound_dir
+console.log('Loading sounds...');
+for (var i in dir) {
+    console.log(dir[i]);
+    regexp += commandName(dir[i]);
+    if (i < dir.length-1) regexp += '|';
+}
+regexp += ')$'
+description += `\nsounds: ${commandName(dir)}`;
+
+// return command name and map command name to sound file
+function commandName(f) {
+    if(Array.isArray(f)) return f.map(commandName);
+    var name = f.substring(0,f.indexOf('\.'));
+    sounds[name] = f;
+    return name;
+}
+
+module.exports = {
+    name: 'sound',
+    description: description,
+    usage: '[sound]',
+    regexp: regexp,
+    action: function(message) {
+        if(message.channel.type === 'dm') return;
+        var voiceChannel = message.member.voiceChannel;
+        var sound = sounds[message.content];
+
+        if(voiceChannel) {
+            voiceChannel.join().then( connection => {
+                var sound_file = `${sound_dir}/${sound}`
+                var dispatcher = connection.playFile(sound_file);
+
+            console.log(`Connected to voice channel: ${voiceChannel.guild} ${voiceChannel.name}${voiceChannel}`);
+            console.log(`Playing ${sound_file}`);
+
+            dispatcher.on('end', () => {
+                console.log('Done playing');
+                connection.disconnect();
+                voiceChannel.leave();
+            });
+
+            }).catch(console.error);
+        }
+    }
+}
